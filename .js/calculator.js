5709b6bdbd85bf0685526d5683ed6e72c24054d9
KAMISENHEIMER <113205388+KAMISENHEIMER@users.noreply.github.com>
2024-04-24 22:04:26 -0500

    added roll function, to be used in future dice calculations

--- .js/calculator.js
+++ .js/calculator.js
@@ -1,16 +1,31 @@
 acBox = document.getElementById("ac");
 amountBox = document.getElementById("amount");
 statblockBox = document.getElementById("statblock");
+resultsText = document.getElementById("results");
+hitsText = document.getElementById("hits");
+damageText = document.getElementById("damage");
 
+
+function roll(num, dice) {
+    sum=0;
+    for (i=0; i<num; i++) {
+        sum += Math.floor(Math.random()*(dice)+1);
+    }
+    return sum;
+}
 
 function calculate() {
     
     ac = acBox.value;
     amount = amountBox.value;
     statblock = statblocks[statblockBox.selectedIndex]
-        
     
+    roll1 = roll(2,6);
+    roll2 = roll(2,6);
     
+    hitsText.textContent = "Hits: " + roll1;
+    damageText.textContent = "Damage: " + roll2;
+
 }
 
 
